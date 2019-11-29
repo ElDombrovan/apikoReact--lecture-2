d29c3ef468171a7ca847ee4fb2bc779fa46d6d0f
El <ElDombrovan@users.noreply.github.com>
2019-11-29 14:02:52 +0200

    Project structure - add component
    Added MoreButton component to the project tree

---
+++ src/components/MoreButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MoreButton() {
+  return (
+    <div className="container-btn--more">
+      <button>Load more</button>
+    </div>
+  );
+}
+
+export default MoreButton;
