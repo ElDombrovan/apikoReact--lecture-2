26a5bd873d074e92560ddfbcff5857f549bb570c
El <ElDombrovan@users.noreply.github.com>
2019-11-29 14:05:11 +0200

    Project structure - add component
    Added Loader component to the project tree

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="container-loader">
+      <span>this is loader</span>
+    </div>
+  );
+}
+
+export default Loader;
